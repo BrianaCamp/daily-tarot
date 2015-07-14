37532c2e7df2ff16727ae65e4c8ecd633d9b9010
Briana Camp <brianarcamp@gmail.com>
2015-07-14 13:27:49 -0700

    simplfying conditionals and fixing indentation

--- js/script.js
+++ js/script.js
@@ -1,93 +1,90 @@
 $(document).ready(function(){
 
+  var buttonClick = 0;
+  var result;
 
-var buttonClick = 0;
-var result;
-
-//count the number of times the button is clicked
-function buttonCount(){
-buttonClick = buttonClick + 1;
+  //count the number of times the button is clicked
+  function buttonCount(){
+    buttonClick = buttonClick + 1;
     console.log(buttonClick);
     return buttonClick;
-}
-
-//randomize the card
-function pickCard() {
-  result = (arrayOfCards[Math.floor(Math.random() * arrayOfCards.length)]);
-  console.log(result.img);
-  $(".category-image img").removeClass("selected");
-  $(".category-image").children("img."+result.category).addClass("selected");
-}
-
-//flip card functionality
-function flipCard() {
-
-  $(".card").flip({
-    axis: 'y',
-    trigger : 'manual',
-    speed : 500
-  });
-  $(".card").flip(true);
-
-}
-
-function putCardInDOM(card) {
-  var html = cardToHtml(card);
-  $(".back img").remove();
-  $(".back").append(html.img).show();
-  $(".name").append(html.name).hide().slideDown();
-  $(".description").append(html.description).hide().slideDown();
-}
-
-function cardToHtml(card) {
-  return {
-    name : "<h2>" + card.name + "</h2>",
-    img : "<img src=" + card.img + ">",
-    description: "<p>" + card.description + "</p>"
-  };
-}
-
-//display the random card and hide it by every click whether even or odd
-function displayImg() {
-  if(buttonClick % 2 !== 0) {
-    putCardInDOM(result);
-    flipCard();
-    }
-    else {
-        $(".front").show();
-        $(".card").flip(false);
-        $(".name h2").slideUp();
-        $(".description p").slideUp();
-
-    }
-    $(".category-image img.selected").show();
-}
-
-//rotate the card if true
-function rotateCard() {
-  var rotate = [true, false],
-  cardRevDescript = "<p> <span class='reversed'>Reversed: </span>" + result.reverse + "</p>";
-  rotate = (rotate[Math.floor(Math.random() * rotate.length)]);
-  console.log(rotate);
-  if (rotate === true && (buttonClick % 2 !== 0)) {
-    $(".back img").addClass("rotate");
-    $(".description").append(cardRevDescript).hide().slideDown();
   }
-}
 
-$(".greeting").hide().fadeIn(1200);
+  //randomize the card
+  function pickCard() {
+    result = (arrayOfCards[Math.floor(Math.random() * arrayOfCards.length)]);
+    console.log(result.img);
+    $(".category-image img").removeClass("selected");
+    $(".category-image").children("img."+result.category).addClass("selected");
+  }
 
-//when the button is clicked, run the buttonCount, pickCard, and displayImg function
+  //flip card functionality
+  function flipCard() {
+    $(".card").flip({
+      axis: 'y',
+      trigger : 'manual',
+      speed : 500
+    });
+    $(".card").flip(true);
+  }
 
-$(".fateButton").click(function(){
-  buttonCount();
-  pickCard();
-  displayImg();
-  rotateCard();
-});
+  function putCardInDOM(card) {
+    var html = cardToHtml(card);
+    $(".back img").remove();
+    $(".back").append(html.img).show();
+    $(".name").append(html.name).hide().slideDown();
+    $(".description").append(html.description).hide().slideDown();
+  }
 
-$(".fateButton").one("click", function(){
-  $("#greeting-message").remove();
-});
+  function cardToHtml(card) {
+    return {
+      name : "<h2>" + card.name + "</h2>",
+      img : "<img src=" + card.img + ">",
+      description: "<p>" + card.description + "</p>"
+    };
+  }
+
+  //display the random card and hide it by every click whether even or odd
+  function displayImg() {
+
+    if(buttonClick % 2) {
+      putCardInDOM(result);
+      flipCard();
+    } else {
+      $(".front").show();
+      $(".card").flip(false);
+      $(".name h2").slideUp();
+      $(".description p").slideUp();
+    }
+
+    $(".category-image img.selected").show();
+  }
+
+  //rotate the card if true
+  function rotateCard() {
+    var rotate = [true, false],
+    cardRevDescript = "<p> <span class='reversed'>Reversed: </span>" + result.reverse + "</p>";
+    rotate = (rotate[Math.floor(Math.random() * rotate.length)]);
+    console.log(rotate);
+    if (rotate && (buttonClick % 2)) {
+      $(".back img").addClass("rotate");
+      $(".description").append(cardRevDescript).hide().slideDown();
+    }
+  }
+
+  $(".greeting").hide().fadeIn(1200);
+
+  //when the button is clicked, run the buttonCount, pickCard, and displayImg function
+
+  $(".fateButton").click(function(){
+    buttonCount();
+    pickCard();
+    displayImg();
+    rotateCard();
+  });
+
+  $(".fateButton").one("click", function(){
+    $("#greeting-message").remove();
+  });
 
 });
